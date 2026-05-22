34bc4cfb5d57f8c4263e336b60d30c9a35b9497a
Mitanshu Thakore <mitanshu.thakore@cern.ch>
2026-05-22 09:58:44 -0500

    Updated bash code

--- plotcommand.sh
+++ plotcommand.sh
@@ -7,6 +7,6 @@ python3 Plotter.py \
        /nfs_scratch/mithakor/ObjectReco_ID_Efficiency/Run2_matching_0_3/RadionTohhTohtatahbb_narrow_M-3000_TuneCP5_13TeV-madgraph-pythia8.root  \
        /nfs_scratch/mithakor/ObjectReco_ID_Efficiency/Run2_matching_0_3/RadionTohhTohtatahbb_narrow_M-4000_TuneCP5_13TeV-madgraph-pythia8.root  \
        /nfs_scratch/mithakor/ObjectReco_ID_Efficiency/Run2_matching_0_3/RadionTohhTohtatahbb_narrow_M-4500_TuneCP5_13TeV-madgraph-pythia8.root  \
-    --ele-skip-cuts "7" \
+    --ele-skip-cuts "5,6,7" \
     --nproc 40  \
     &> plotting_run2_eff.log &
