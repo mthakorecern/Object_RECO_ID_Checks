6d193408b2790e8b344f410e67660140d4ff77fb
Mitanshu Thakore <mitanshu.thakore@cern.ch>
2026-06-27 06:32:44 -0500

    updated

--- plotcommand.sh
+++ plotcommand.sh
@@ -7,7 +7,7 @@ python3 Plotter.py \
        /nfs_scratch/mithakor/ObjectReco_ID_Efficiency/AddedHEEPID/GluGlutoRadiontoHHto2B2Tau_M-3000_narrow_TuneCP5_13p6TeV_madgraph-pythia8.root  \
        /nfs_scratch/mithakor/ObjectReco_ID_Efficiency/AddedHEEPID/GluGlutoRadiontoHHto2B2Tau_M-4000_narrow_TuneCP5_13p6TeV_madgraph-pythia8.root  \
        /nfs_scratch/mithakor/ObjectReco_ID_Efficiency/AddedHEEPID/GluGlutoRadiontoHHto2B2Tau_M-4500_narrow_TuneCP5_13p6TeV_madgraph-pythia8.root  \
-    --ele-skip-cuts "7" \
+    --ele-skip-cuts "5,7" \
     --tau-decay-modes ""  \
     --boosted-tau-decay-modes ""\
     --require-tau-newdm-id  \
